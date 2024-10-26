fc13395085f2c3b9d444bcb9c9f363e76a78876b
xprchlik <erikson006@gmail.com>
2024-10-26 16:43:58 +0200

    Design adjustment

--- app/src/main/java/sk/sivy_vlk/zazipovazie/SplashScreenActivity.kt
+++ app/src/main/java/sk/sivy_vlk/zazipovazie/SplashScreenActivity.kt
@@ -7,18 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_splash_screen)
-//        val r = Runnable {
-//            // Start your app main activity
-//            val i = Intent(
-//                this@SplashScreenActivity,
-//                MainActivity::class.java
-//            )
-//            startActivity(i)
-//            // close this activity
-//            finish()
-//        }
-//        Handler(Looper.getMainLooper()).postDelayed(r, 3000)
         startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
         finish()
     }
