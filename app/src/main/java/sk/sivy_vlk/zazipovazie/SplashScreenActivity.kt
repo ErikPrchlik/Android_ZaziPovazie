22465e66e8a792e4e7b0962089ee60bf94010780
xprchlik <erikson006@gmail.com>
2024-10-29 01:06:53 +0100

    Full Screen adjustment

--- app/src/main/java/sk/sivy_vlk/zazipovazie/SplashScreenActivity.kt
+++ app/src/main/java/sk/sivy_vlk/zazipovazie/SplashScreenActivity.kt
@@ -2,6 +2,9 @@ package sk.sivy_vlk.zazipovazie
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
